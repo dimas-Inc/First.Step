be8b6342b52889ea2ab8d3e4fafc604e1c909f63
dimas-Inc <71440589+dimas-Inc@users.noreply.github.com>
2020-10-07 02:31:09 +0300

    task upd

--- SRC/HomeWork_4/Task_1.2.py
+++ SRC/HomeWork_4/Task_1.2.py
@@ -1,9 +1,14 @@
-a = [3, 10, 5, 25, 2, 8]
-for i in a:
-    b = i^a[0] 
-    c = i^a[1]
-    d = i^a[2]
-    f = i^a[3]
-    g = i^a[4]
-    h = i^a[5]
-    print(c)
+s=[3, 10, 5, 25, 2]
+z=[]
+for i in s:
+    a=i^s[0]
+    z.append(a)
+    b=i^s[1]
+    z.append(b)
+    c=i^s[2]
+    z.append(c)
+    d=i^s[3]
+    z.append(d)
+    e=i^s[4]
+    z.append(e)
+print(max(z))
--- SRC/HomeWork_4/Task_2.1.py
+++ SRC/HomeWork_4/Task_2.1.py
@@ -1,10 +1,18 @@
-z = [2,3,5,6]
+z = [2,7,11,15]
 x = 9
-ind = []
 for i in range(0,len(z)):
-    for j in range(0,len(z)):
+    for j in range(i+1,len(z)):
         if z[i]+z[j] == x:
-            ind.append(i)
-            ind.append(j)
-print(ind)
-            
+            print(i,j)
+z1 = [3,2,4]
+X1 = 6
+for i in range(0,len(z1)):
+    for j in range(i+1,len(z1)):
+        if z1[i]+z1[j] == X1:
+            print(i,j)
+z2 = [3,3]
+X2 = 6
+for i in range(0,len(z2)):
+    for j in range(i+1,len(z2)):
+        if z2[i]+z2[j] == X2:
+            print(i,j)
